3928fa6095e859c3b88c9757a71d13ba19a74e22
kirubala <kirubala@KIRUBALA-IN.oradev.oraclecorp.com>
2020-02-13 20:05:43 +0530

    Updates

---
+++ precache-manifest.daa9814399df94d21d95efa7605c9ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3a47f7ceec04e4b6548",
+    "url": "/React-Todo-list.git/static/css/main.d45c78c2.chunk.css"
+  },
+  {
+    "revision": "c3a47f7ceec04e4b6548",
+    "url": "/React-Todo-list.git/static/js/main.f311e41b.chunk.js"
+  },
+  {
+    "revision": "b94df0381d7c09855595",
+    "url": "/React-Todo-list.git/static/js/runtime~main.6edebde2.js"
+  },
+  {
+    "revision": "d3f659e1b64b06bf6105",
+    "url": "/React-Todo-list.git/static/js/2.ccd57a61.chunk.js"
+  },
+  {
+    "revision": "cfcd9e9395b867e7a9b219a164d4b90b",
+    "url": "/React-Todo-list.git/index.html"
+  }
+];
